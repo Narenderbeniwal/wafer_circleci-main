61d4e01a3b6ec2d4acb4ce79f9d16603ad6a45a3
Narender Kumar <narenderbeniwal1234@gmail.com>
2021-11-23 08:19:35 +0530

    first commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running"
+    return "Flask app is running and i am changing something"
 
 
 if __name__ == "__main__":
