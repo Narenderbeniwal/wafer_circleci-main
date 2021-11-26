8b158af879ba0e71c5ed2214e8fc3314555cc0ed
Narender Kumar <narenderbeniwal1234@gmail.com>
2021-11-26 07:40:49 +0530

    first commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running, that's cook"
+    return "Flask app is running, that's cool"
 
 port = int(os.getenv("PORT", 5001))
 
