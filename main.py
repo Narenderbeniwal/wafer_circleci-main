5466672857a1173ccbe2f214ca6a7198f5881ea0
Narender Kumar <narenderbeniwal1234@gmail.com>
2021-11-27 08:44:58 +0530

    port updated

--- main.py
+++ main.py
@@ -95,10 +95,10 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successful!!")
 
-# port = int(os.getenv("PORT",5000))
+port = int(os.getenv("PORT",5000))
 if __name__ == "__main__":
     host = '0.0.0.0'
-    port = 5001
+    #port = 5001
     httpd = simple_server.make_server(host, port, app)
     # print("Serving on %s %d" % (host, port))
     httpd.serve_forever()
