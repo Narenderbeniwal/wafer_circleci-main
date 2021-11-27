518bbe66f931661b9496f9ba6f154f64e3c28dbc
Narender Kumar <narenderbeniwal1234@gmail.com>
2021-11-27 08:23:02 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="docker-heroku",
+    name="wafer-fault-detection",
     version="0.0.3",
     description="ML project",
     author="Avnish yadav", 
